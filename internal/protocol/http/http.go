fd06f35b4dab132c71e52e5a84ad32f6f69bb9ab
gidyon <gideonhacer@gmail.com>
2019-07-07 17:18:06 +0300

    changed package to http as its more appropriate, improved documentation

--- internal/protocol/http/http.go
+++ internal/protocol/http/http.go
@@ -1,4 +1,4 @@
-package rest
+package http
 
 import (
 	"context"
@@ -138,7 +138,6 @@ func Serve(
 // connections or otherHandler otherwise. Copied from cockroachdb.
 func grpcHandlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// TODO(tamird): point to merged gRPC code rather than a PR.
 		// This is a partial recreation of gRPC's internal checks https://github.com/grpc/grpc-go/pull/514/files#diff-95e9a25b738459a2d3030e1e6fa2a718R61
 		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
 			grpcServer.ServeHTTP(w, r)
